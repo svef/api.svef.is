bf6645fef5996025779950698aeb7d4e681e6df9
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-05-18 23:39:44 +0000

    Fix lint error in src/index

--- src/server.js
+++ src/server.js
@@ -5,10 +5,8 @@ const router = require('koa-router')()
 const app = koa()
 
 const glob = require('glob')
-glob.sync(path.resolve(process.cwd(), './src/routes/*.js')).forEach((route) => {
-  console.log(route)
-  require(route)(router)
-})
+// eslint-disable-next-line global-require
+glob.sync(path.resolve(process.cwd(), './src/routes/*.js')).forEach((route) => require(route)(router))
 
 app
   .use(router.routes())
