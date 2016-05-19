a0ee6cd9298aad516c652bcd0aa9d3b2f5081fc5
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2016-05-19 19:09:14 +0000

    Add CORS support

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const koa = require('koa')
 const router = require('koa-router')()
 const bodyParser = require('koa-bodyparser')
+const cors = require('kcors')
 const glob = require('glob')
 
 const app = koa()
@@ -10,6 +11,7 @@ const app = koa()
 glob.sync(path.resolve(process.cwd(), './src/routes/*.js')).forEach((route) => require(route)(router))
 
 app
+  .use(cors())
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods())
