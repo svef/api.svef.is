d5bbb91f011571fbf2a848c24cd7d3fd50895508
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2016-05-19 19:21:18 +0000

    Add post url

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,13 +1,15 @@
 const { get } = require('axios')
 
+const mediumUrl = 'https://medium.com/vefidnadurinn/'
+
 module.exports = (router) => {
   router.get('/posts', function* () {
-    const { data } = yield get('https://medium.com/vefidnadurinn/latest?format=json')
+    const { data } = yield get(`${mediumUrl}latest?format=json`)
     const json = JSON.parse(data.substr('])}while(1);</x>'.length))
     this.body = json.payload.posts.map((post) => {
       return {
         title: post.title,
-        slug: post.slug,
+        url: `${mediumUrl}${post.uniqueSlug}`,
         content: post.previewContent.bodyModel.paragraphs[1].text,
         publishedAt: post.firstPublishedAt,
       }
